9b819acd76943f7a3df22882107bb55802964bc2
pc <pc@192.168.100.7>
2019-04-07 00:58:46 +0530

    3 commit

---
+++ TestProj1/src/Inner.java
@@ -0,0 +1,20 @@
+
+
+
+ class Inner 
+{
+
+	 public static void  main(String args[]) 
+	{
+		System.out.println("main inside Outer class");
+
+	}
+	 class Outer
+	 {
+		  void mai()
+		 {
+			 System.out.println("hello Inner");
+		 }
+	 }
+
+}
